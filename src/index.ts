e7de6a90412d114a4e40a298a3dbf6e934cc5bb1
Michal Podworny <podworny.m@gmail.com>
2020-02-12 21:22:02 +0100

    a bit prettier colours

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 class Color3 {
-  constructor(public r: number, public g: number, public b: number) { }
+  constructor(public r: number, public g: number, public b: number) {}
 }
 const color3 = (r: number, g: number, b: number) => new Color3(r, g, b);
 
@@ -8,6 +8,7 @@ const DOMAIN_END_X = 1;
 const DOMAIN_START_Y = -1;
 const DOMAIN_END_Y = 1;
 const MAX_ITERATIONS = 512;
+const MAX_ITERATIONS_LOG = Math.log(MAX_ITERATIONS);
 
 const resizeCanvas = (canvas: HTMLCanvasElement): void => {
   const displayWidth = canvas.clientWidth;
@@ -19,7 +20,7 @@ const resizeCanvas = (canvas: HTMLCanvasElement): void => {
   }
 };
 
-const scale1d = (startD1: number, endD1: number, startD2: number, endD2: number, value: number) => {
+const scaleDomain = (startD1: number, endD1: number, startD2: number, endD2: number, value: number) => {
   const magD1 = endD1 - startD1;
   const magD2 = endD2 - startD2;
 
@@ -48,17 +49,16 @@ const mandelbrotCheck = (x0: number, y0: number): number | null => {
   return null;
 };
 
-const mandelbrotColor = (x: number, y: number): Color3 => {
-  const check = mandelbrotCheck(x, y);
+const mandelbrotColor = (check: number | null): Color3 => {
   if (check === null) {
     return color3(0, 0, 0);
   } else {
-    const greyscale = Math.floor(scale1d(0, MAX_ITERATIONS - 1, 0, 255, MAX_ITERATIONS - check));
-    return color3(greyscale, greyscale, greyscale);
+    const checkSmoothed = scaleDomain(0, MAX_ITERATIONS_LOG, 0, 255, Math.log(check + 1));
+    return color3(checkSmoothed, checkSmoothed, checkSmoothed);
   }
 };
 
-const mandelbrotPixels = function* (width: number, height: number) {
+const mandelbrotPixels = function*(width: number, height: number) {
   let i = 0;
   const pixelCount = width * height;
 
@@ -66,10 +66,11 @@ const mandelbrotPixels = function* (width: number, height: number) {
     const pixY = Math.floor(i / width);
     const pixX = i - pixY * width;
 
-    const scaledX = scale1d(0, width - 1, DOMAIN_START_X, DOMAIN_END_X, pixX);
-    const scaledY = scale1d(0, height - 1, DOMAIN_START_Y, DOMAIN_END_Y, pixY);
+    const scaledX = scaleDomain(0, width - 1, DOMAIN_START_X, DOMAIN_END_X, pixX);
+    const scaledY = scaleDomain(0, height - 1, DOMAIN_START_Y, DOMAIN_END_Y, pixY);
 
-    const color = mandelbrotColor(scaledX, scaledY);
+    const check = mandelbrotCheck(scaledX, scaledY);
+    const color = mandelbrotColor(check);
 
     yield color.r;
     yield color.g;
